a9b0752e2eb4643767da2618d91384a1320efd2e
Laxmi Nag L N <laxminagln@gmail.com>
2020-01-19 22:03:55 +0530

    Update 1. image read and show code.py

--- 1. image read and show code.py
+++ 1. image read and show code.py
@@ -7,3 +7,8 @@ img = cv2.imread('address of file',0)
 cv2.imshow('image',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+# description
+# The import keyword is used to import library
+# a variable 'img' is stored with an image specified through the address. The function used is 'imread()'
+# 0 value refers to gray and 1 refers to color
